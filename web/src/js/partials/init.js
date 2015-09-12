91690dde41ee6bd9ad63368730fc5b0ade81c5b6
rSamokhin <poshliemail@googlemail.com>
2015-09-12 13:09:45 +0300

    init

---
+++ web/src/js/partials/init.js
@@ -0,0 +1,5 @@
+Object.keys(window.Handlers).forEach(function (bindFunctionEvent) {
+    Object.keys(window.Handlers[bindFunctionEvent]).forEach(function (bindFunctionName) {
+        $(document.body).on(bindFunctionEvent, '[data-bind-'+bindFunctionEvent+'*='+bindFunctionName+']', window.Handlers[bindFunctionEvent][bindFunctionName]);
+    });
+});
