c439fcc24c46be9d3854f8b963e7356a60a934c9
rSamokhin <poshliemail@googlemail.com>
2015-09-12 13:10:30 +0300

    fix

--- web/src/js/partials/init.js
+++ web/src/js/partials/init.js
@@ -3,3 +3,6 @@ Object.keys(window.Handlers).forEach(function (bindFunctionEvent) {
         $(document.body).on(bindFunctionEvent, '[data-bind-'+bindFunctionEvent+'*='+bindFunctionName+']', window.Handlers[bindFunctionEvent][bindFunctionName]);
     });
 });
+window.Handlers = {
+    click: {}
+}
